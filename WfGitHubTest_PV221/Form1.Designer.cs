2327c28ba438fe12d3a3279454b16afc1279af07
Ivan Kyryllov <Kiri_wh11@student.itstep>
2023-10-26 20:17:01 +0300

    Add random quotes generation

--- WfGitHubTest_PV221/Form1.Designer.cs
+++ WfGitHubTest_PV221/Form1.Designer.cs
@@ -29,32 +29,47 @@
         private void InitializeComponent()
         {
             lblData = new Label();
+            btnPush = new Button();
             SuspendLayout();
             // 
             // lblData
             // 
             lblData.BackColor = Color.FromArgb(255, 255, 192);
             lblData.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
-            lblData.Location = new Point(179, 126);
+            lblData.Location = new Point(177, 112);
             lblData.Name = "lblData";
             lblData.Size = new Size(416, 162);
             lblData.TabIndex = 0;
             lblData.Text = "Hello, World";
             lblData.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // btnPush
+            // 
+            btnPush.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPush.Location = new Point(303, 332);
+            btnPush.Name = "btnPush";
+            btnPush.Size = new Size(164, 75);
+            btnPush.TabIndex = 1;
+            btnPush.Text = "Push";
+            btnPush.UseVisualStyleBackColor = true;
+            btnPush.Click += btnPush_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnPush);
             Controls.Add(lblData);
             Name = "Form1";
             Text = "GitHub Test";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
 
         private Label lblData;
+        private Button btnPush;
     }
 }
